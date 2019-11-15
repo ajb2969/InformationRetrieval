46d2904f2f0cdc85f6ad3bec93fc026ac5155689
alexbrown <ajb2969@rit.edu>
2019-11-15 16:39:44 -0500

    created home page, added static resources and mapped them to java spring application

--- src/main/java/query/QueryController.java
+++ src/main/java/query/QueryController.java
@@ -1,11 +1,7 @@
 package query;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class QueryController {
