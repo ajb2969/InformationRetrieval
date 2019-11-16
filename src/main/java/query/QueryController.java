c1809440028a44b8b37c8544f4e6445bee045e54
alexbrown <ajb2969@rit.edu>
2019-11-15 19:16:54 -0500

    removed unused dependencies. Resolved issues with posting information

--- src/main/java/query/QueryController.java
+++ src/main/java/query/QueryController.java
@@ -1,19 +1,28 @@
 package query;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class QueryController {
 
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("query", new query.Querycontainer());
         return "index";
     }
 
-    @GetMapping("/error")
-    public String error() {
-        return "An error has occurred";
+    @PostMapping("/query")
+    public String querySubmit(@ModelAttribute("query") Querycontainer query, BindingResult bindingResult, Model model) {
+        if(query != null) {
+            System.out.println("The query was " + query.getContent());
+        }
+        return "result";
     }
 
 }
