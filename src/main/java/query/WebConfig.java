09d212f6ba1c5e4a28aca8ddb2f71932b568ea16
alexbrown <ajb2969@rit.edu>
2019-11-17 16:39:46 -0500

    resolved page loading issue

--- src/main/java/query/WebConfig.java
+++ src/main/java/query/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
