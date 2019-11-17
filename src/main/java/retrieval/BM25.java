f556f5febba9ed5dc734671e817019316e0ed80d
cyork <cxy1054@rit.edu>
2019-11-17 10:59:23 -0500

    gm25 work in progress

--- src/main/java/retrieval/BM25.java
+++ src/main/java/retrieval/BM25.java
@@ -1,18 +1,38 @@
 package retrieval;
 
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BM25 extends Models {
+
+    private Map<String, Entry> index;
+
     public BM25() {
         super();
+        this.index = super.get_doc_indicies();
     }
 
     @Override
     public ArrayList<String> retrieve(String query) {
         System.out.println("Executed BM25");
-        HashMap<String, Entry> index = super.get_doc_indicies();
-        String[] keywords = super.extractTerms(query);
+        ArrayList<String> keywords = Lists.newArrayList(super.extractTerms(query));
         return null;
+
+    }
+
+    private double getScore(List<String> keywords, String filename) {
+        double sum = 0;
+        sum = keywords.stream()
+            .mapToDouble(this::getScore)
+            .sum();
+        return sum;
+
+    }
+
+    private double getScore(String term) {
+        return 0.0;
     }
 }
