58ba7656f7ed8cd5a7eb1a2f2c56b64df7965a96
alexbrown <ajb2969@rit.edu>
2019-11-17 13:48:21 -0500

    added document previews to results

--- src/main/java/query/QueryController.java
+++ src/main/java/query/QueryController.java
@@ -17,20 +17,23 @@ import retrieval.BM25;
 import retrieval.Models;
 import retrieval.TfIdf;
 
-import javax.print.DocFlavor;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Controller
 public class QueryController {
     private final String documentsPath = "documents/";
+    final File temp = new File(documentsPath + "temp.txt");
+
     @GetMapping("/")
     public String welcome(Model model) {
         model.addAttribute("query", new query.Querycontainer());
@@ -59,6 +62,9 @@ public class QueryController {
     public String querySubmit(@ModelAttribute("query") Querycontainer query,
                               Model model) {
         if (query != null) {
+            if(temp.exists()) {
+                temp.delete();
+            }
             Models m =
                     query.getSelectedModel().toLowerCase().equals("tfidf") ?
                             new TfIdf() : new BM25();
@@ -68,8 +74,9 @@ public class QueryController {
             for (TfIdf.Similarity sim : documents) {
                 sim.setPreview(getLongestIncreasingSequence(sim.getDocument_name(), query.getContent()));
             }
-            //if the first document isn't relevant or similar send back no results available
-            if(documents.get(0).getSimilarity() == 1) {
+            //if the first document isn't relevant or similar send back no
+            // results available
+            if (documents.get(0).getSimilarity() == 1) {
                 model.addAttribute("results", new ArrayList<>());
             } else {
                 model.addAttribute("results", documents);
@@ -82,23 +89,68 @@ public class QueryController {
 
     @GetMapping("/document")
     @ResponseBody
-    public FileSystemResource getDocument(@RequestParam(required = true) String doc,
-                                          Model model) {
-        return new FileSystemResource(new File(documentsPath + doc));
+    public FileSystemResource getDocument(@RequestParam(required = true) String doc, Model model) {
+        try {
+            if (temp.exists()) {
+                temp.delete();
+            }
+            List<String> selected =
+                    Files.readAllLines(Paths.get(documentsPath + doc));
+            selected =
+                    selected.stream().map(e -> e + "<br>").collect(Collectors.toList());
+            Files.write(Paths.get(String.valueOf(temp)), selected);
+            return new FileSystemResource(documentsPath + "temp.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new FileSystemResource(documentsPath + doc);
     }
 
     private String getLongestIncreasingSequence(String docName, String query) {
         try {
-            String file = new String(Files.readAllBytes(Paths.get(documentsPath + docName)));
+            final int WINDOWSIZE = 20;
+            int currStart = 0;
+            String file =
+                    new String(Files.readAllBytes(Paths.get(documentsPath + docName)));
             file = file.replaceAll("<[^>]*>", "");
-            ArrayList<String> tokens = (ArrayList<String>) Arrays.stream(file.split(" "))
-                    .filter(token -> !token.isEmpty() || !token.equals(""))
-                    .map(token -> token.replaceAll("[.,!?:\\[\\]]", ""))
-                    .map(String::toLowerCase)
-                    .collect(Collectors.toList());
+            // get file tokens
+            ArrayList<String> tokens =
+                    (ArrayList<String>) Arrays.stream(file.split(" ")).collect(Collectors.toList());
+            // get query terms
+            String[] terms = query.split(" ");
 
+            int maxTerms = 0;
+            String[] bestWindow = new String[0];
+            do {
+                //populate window
+                String[] window = IntStream.range(currStart,
+                        currStart + 20 < tokens.size() ?
+                                currStart + WINDOWSIZE :
+                                tokens.size() - currStart).mapToObj(tokens::get).map(String::toLowerCase).filter(token -> !token.isEmpty() || !token.equals(""))
+                        .map(token -> token.replaceAll("[.,!?:\\[\\]]", "")).collect(Collectors.toList()).toArray(new String[WINDOWSIZE]);
+                if (maxTerms == terms.length) {
+                    bestWindow = window;
+                    break;
+                } else {
+                    int contains = 0;
+                    for (String term : terms) {
+                        if (Arrays.asList(window).contains(term.toLowerCase())) {
+                            contains += 1;
+                        }
+                    }
+                    if (contains > maxTerms) {
+                        maxTerms = contains;
+                        bestWindow = window;
+                    }
+                    currStart += 1;
+                }
+            } while (currStart + WINDOWSIZE < tokens.size());
+            if (maxTerms == 0) {
+                return tokens.stream().limit(WINDOWSIZE).collect(Collectors.joining(" "));
+            }
 
-
+            return Arrays.stream(bestWindow).map(Object::toString)
+                    .collect(Collectors.joining(" "));
 
         } catch (IOException e) {
             e.printStackTrace();
